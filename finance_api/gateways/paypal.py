06dca4048b62a9211e339227b78df7d809e85760
Renne Rocha <renne@rennerocha.com>
2020-04-13 17:22:25 -0300

    Remove user-agent validation

--- finance_api/gateways/paypal.py
+++ finance_api/gateways/paypal.py
@@ -27,17 +27,11 @@ def paypal_handshake(ipn_message):
         logger.error("Invalid message sent to Paypal.")
 
 
-
 @router.post("/gateway/paypal")
 async def paypal(request: Request):
-    user_agent = request.headers.get("user-agent")
-    if user_agent != settings.PAYPAL_REQUEST_USER_AGENT:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
-
     form = await request.form()
     receiver_email = form.get("receiver_email")
     if receiver_email != settings.PAYPAL_ACCOUNT:
-        logger.error("Received notification from invalid PayPal account.")
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
 
     paypal_handshake(dict(form))
--- tests/gateways/test_paypal.py
+++ tests/gateways/test_paypal.py
@@ -12,65 +12,43 @@ def ipn_message():
     }
 
 
-@pytest.fixture
-def headers():
-    return {"User-Agent": settings.PAYPAL_REQUEST_USER_AGENT}
-
-
-def test_empty_post_to_paypal_gateway(client, headers):
-    response = client.post("/gateway/paypal", headers=headers)
+def test_empty_post_to_paypal_gateway(client):
+    response = client.post("/gateway/paypal")
     assert response.status_code == 400
 
 
-def test_post_with_incorrect_paypal_user_agent_accepted(client, ipn_message):
-    response = client.post(
-        "/gateway/paypal",
-        headers={"User-Agent": "Not Paypal User-Agent"},
-        data=ipn_message,
-    )
-    assert response.status_code == 400
-
-
-def test_post_with_correct_paypal_user_agent_accepted(
-    mocker, client, headers, ipn_message
-):
+def test_post_with_correct_paypal_user_agent_accepted(mocker, client, ipn_message):
     paypal_handshake = mocker.patch("finance_api.gateways.paypal.paypal_handshake")
 
-    response = client.post("/gateway/paypal", headers=headers, data=ipn_message,)
+    response = client.post("/gateway/paypal", data=ipn_message,)
     assert response.status_code == 200
 
 
-def test_valid_paypal_request_trigger_handshake_process(
-    mocker, client, headers, ipn_message
-):
+def test_valid_paypal_request_trigger_handshake_process(mocker, client, ipn_message):
     paypal_handshake = mocker.patch("finance_api.gateways.paypal.paypal_handshake")
 
-    response = client.post("/gateway/paypal", headers=headers, data=ipn_message,)
+    response = client.post("/gateway/paypal", data=ipn_message,)
 
     paypal_handshake.assert_called_with(ipn_message)
 
 
-def test_raise_error_if_request_to_not_intended_account(
-    mocker, client, headers, ipn_message
-):
+def test_raise_error_if_request_to_not_intended_account(mocker, client, ipn_message):
     ipn_message["receiver_email"] = "not_expected@account.com"
-    response = client.post("/gateway/paypal", headers=headers, data=ipn_message,)
+    response = client.post("/gateway/paypal", data=ipn_message,)
     assert response.status_code == 400
 
 
 def test_do_not_trigger_handshake_process_if_not_intended_account(
-    mocker, client, headers, ipn_message
+    mocker, client, ipn_message
 ):
     paypal_handshake = mocker.patch("finance_api.gateways.paypal.paypal_handshake")
 
     ipn_message["receiver_email"] = "not_expected@account.com"
-    response = client.post("/gateway/paypal", headers=headers, data=ipn_message,)
+    response = client.post("/gateway/paypal", data=ipn_message,)
     assert not paypal_handshake.called
 
 
-def test_paypal_handshake_send_request_to_verify_url(
-    mocker, client, ipn_message
-):
+def test_paypal_handshake_send_request_to_verify_url(mocker, client, ipn_message):
     mock_post_request = mocker.patch("finance_api.gateways.paypal.requests.post")
     paypal_handshake(ipn_message)
 
@@ -83,17 +61,23 @@ def test_paypal_after_verified_store_notification_content(mocker, client, ipn_me
     mock_response = mocker.patch("finance_api.gateways.paypal.requests.post")
     mock_response.return_value.text = "VERIFIED"
 
-    mock_store_notification = mocker.patch("finance_api.gateways.paypal.store_notification")
+    mock_store_notification = mocker.patch(
+        "finance_api.gateways.paypal.store_notification"
+    )
 
     paypal_handshake(ipn_message)
 
     mock_store_notification.assert_called_with(ipn_message)
 
 
-def test_log_error_when_paypal_replied_with_invalid_message_received(mocker, client, ipn_message):
+def test_log_error_when_paypal_replied_with_invalid_message_received(
+    mocker, client, ipn_message
+):
     mock_response = mocker.patch("finance_api.gateways.paypal.requests.post")
     mock_response.return_value.text = "INVALID"
-    mock_store_notification = mocker.patch("finance_api.gateways.paypal.store_notification")
+    mock_store_notification = mocker.patch(
+        "finance_api.gateways.paypal.store_notification"
+    )
     mock_logger_error = mocker.patch("finance_api.gateways.paypal.logger.error")
 
     paypal_handshake(ipn_message)
